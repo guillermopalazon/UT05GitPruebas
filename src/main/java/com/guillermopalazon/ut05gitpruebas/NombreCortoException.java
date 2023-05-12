fc62724c8164e2a1f3a62e1b0e6c63f2c235efa4
guillermopalazon <74924323+guillermopalazon@users.noreply.github.com>
2023-05-12 12:23:43 +0200

    Update NombreCortoException.java

--- src/main/java/com/guillermopalazon/ut05gitpruebas/NombreCortoException.java
+++ src/main/java/com/guillermopalazon/ut05gitpruebas/NombreCortoException.java
@@ -10,6 +10,6 @@ package com.guillermopalazon.ut05gitpruebas;
  */
 public class NombreCortoException extends RuntimeException {
     public String getMessage(){
-        return "El nombre es muy corto, hasta luego";
+        return "El nombre es muy corto, hasta luego Mari Carmen";
     }
 }
